6530c5fb35fa03879f7623863a3436ee434e3d37
PeterIregi <iregipeter4@gmail.com>
2026-06-01 02:15:24 +0300

    Console.log practice

---
+++ consolelog.js
@@ -0,0 +1,11 @@
+console.log("hello World!");
+
+let num = 5;
+console.log(num); //5
+
+
+//more examples of console.log  
+
+let name = "Alice";
+let age = 25;
+console.log("Name: ", name, "Age: ",age);
