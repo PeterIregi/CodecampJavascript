972eabe9ce863a8a9aad9fbcc13fb54714aaaa0d
Peter Iregi <iregipeter4@gmail.com>
2026-06-02 08:51:45 +0300

    Declare and reassign variable value

--- triviaBot.js
+++ triviaBot.js
@@ -7,3 +7,9 @@ console.log("My favorite programming language is "+favoriteLanguage);
 
 let codingFact= favoriteLanguage+" was created in just 10 days by programmer Brendan Eich in May 1995 ";
 console.log(codingFact);
+
+codingFact= favoriteLanguage+" was originally named Mocha, briefly changed to LiveScript, and finally rebranded to "+favoriteLanguage;
+console.log(codingFact)
+
+codingFact = "In "+favoriteLanguage+" Not-a-Number Is a Number";
+console.log(codingFact);
