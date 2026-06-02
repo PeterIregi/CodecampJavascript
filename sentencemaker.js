e26563e20a96f2cbfd9d4bbead0bc9bbbeeedc79
Peter Iregi <iregipeter4@gmail.com>
2026-06-02 11:07:21 +0300

    Assign values to declared variables

--- sentencemaker.js
+++ sentencemaker.js
@@ -3,4 +3,12 @@ let noun;
 let verb;
 let place;
 let adjective2;
-let noun
+let noun2;
+
+adjective= "large";
+noun = "girl";
+verb ="ate";
+place ="kitchen"
+adjective2 = "beautiful";
+noun2 = "lady";
+
