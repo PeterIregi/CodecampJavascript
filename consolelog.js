791365ff0fc89be7ed369fa412b03cd4b770fd06
PeterIregi <iregipeter4@gmail.com>
2026-06-02 22:14:57 +0300

    Modify for youtube

--- consolelog.js
+++ consolelog.js
@@ -1,11 +1,27 @@
+// this is the first day of lerning javascript 
+
+//this is how we diplay to the console using the console.log function 
+
+//here is an example of the various results 
+
 console.log("hello World!");
+//will display hello world!
+
+
 
 let num = 5;
 console.log(num); //5
-
+//will display the value associated with the variable we just declared of num
 
 //more examples of console.log  
 
 let name = "Alice";
 let age = 25;
 console.log("Name: ", name, "Age: ",age);
+
+//this will display the string name and the value associated with the variable name 
+
+//if we run our code it should display as expected
+
+
+//it displays hello World!, 5 and name Alice and the rest as it should next we will see how to join two or more strings or a string with a variable that could be a string or any other data type 
