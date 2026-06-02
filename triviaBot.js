484edeb928a8383406a2a5c63b8d9be057214b64
Peter Iregi <iregipeter4@gmail.com>
2026-06-02 08:49:38 +0300

    Declare and console log variables

--- triviaBot.js
+++ triviaBot.js
@@ -1,1 +1,4 @@
 console.log("Hello! I'm your coding fun fact guide!");
+let botName ="TriviaBot", botLocation = "Kenya", favoriteLanguage ="Javascript";
+
+console.log("My name is "+botName+" and I live on "+botLocation);
