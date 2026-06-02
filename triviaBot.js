5205ec3e2b3fbcf919a0ed9a61260897fd68ff17
Peter Iregi <iregipeter4@gmail.com>
2026-06-02 08:50:41 +0300

    Console log and declare new variable

--- triviaBot.js
+++ triviaBot.js
@@ -2,3 +2,8 @@ console.log("Hello! I'm your coding fun fact guide!");
 let botName ="TriviaBot", botLocation = "Kenya", favoriteLanguage ="Javascript";
 
 console.log("My name is "+botName+" and I live on "+botLocation);
+
+console.log("My favorite programming language is "+favoriteLanguage);
+
+let codingFact= favoriteLanguage+" was created in just 10 days by programmer Brendan Eich in May 1995 ";
+console.log(codingFact);
