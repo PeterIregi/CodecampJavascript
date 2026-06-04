f6ece5781b8fae73d4436f18f65d5a9e4a7cb2e2
Peter Iregi <iregipeter4@gmail.com>
2026-06-04 08:39:41 +0300

    Sentence maker

--- sentencemaker.js
+++ sentencemaker.js
@@ -14,3 +14,5 @@ noun2 = "lady";
 
 
 let firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("First story: "+firstStory);
