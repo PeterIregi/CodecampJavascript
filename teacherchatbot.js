e6a867981d6f256ed96b98251680d0982d2fb0cd
Peter Iregi <iregipeter4@gmail.com>
2026-06-15 10:45:57 +0300

    Create another format string

--- teacherchatbot.js
+++ teacherchatbot.js
@@ -6,5 +6,9 @@ let botName ="teacherBot";
 let greeting=`My name is ${botName}.`;
 console.log(greeting);
 //step 4
-let subject = "Javascript";
+let subject = "JavaScript";
 let topic = "strings";
+//step 5
+let sentence = `Today, you will learn about ${topic} in ${subject}.`;
+
+console.log(sentence);
