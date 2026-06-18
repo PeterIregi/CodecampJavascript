1ad1e5619e74dafc4035befb435e78829daf5b7b
Peter Iregi <iregipeter4@gmail.com>
2026-06-18 11:16:01 +0300

    check if substrin is in string

--- stringinspector.js
+++ stringinspector.js
@@ -2,3 +2,5 @@
 let fccSentence = "freeCodeCamp is a great place to learn web development."
 //step 2
 console.log("Here are some examples of the includes() method:")
+//step 3
+let hasFreeCodeCamp =fccSentence.includes("freeCodeCamp");
