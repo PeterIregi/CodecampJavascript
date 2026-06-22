c0cb2442f716993d7427c060e3e70da246a3f951
Peter Iregi <iregipeter4@gmail.com>
2026-06-22 10:22:20 +0300

    Display result of trim method

--- stringformatter.js
+++ stringformatter.js
@@ -5,4 +5,7 @@ console.log("Original input:");
 //step 3
 console.log(userInput);
 //step 4
-let cleanedInput = userInput.trim()
+let cleanedInput = userInput.trim();
+//step 5
+console.log("Result of trimming whitespace from both ends:");
+console.log(cleanedInput);
