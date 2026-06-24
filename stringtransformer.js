fb53a81ebbbfb6adb9e97fc791e42f783e816ce1
Peter Iregi <iregipeter4@gmail.com>
2026-06-24 10:56:44 +0300

    Use the replace method

--- stringtransformer.js
+++ stringtransformer.js
@@ -1,2 +1,7 @@
 //step 1
 let originalString = "I love cats.";
+//step 2
+console.log("Original string:");
+console.log(originalString)
+//step 3
+let replacedString = originalString.replace("cats", "dogs");
