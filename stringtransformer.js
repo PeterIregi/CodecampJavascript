283ac469fa33e7c40522fbfe07347dc5c006a31b
Peter Iregi <iregipeter4@gmail.com>
2026-06-24 10:59:14 +0300

    Display the Use the replace method

--- stringtransformer.js
+++ stringtransformer.js
@@ -2,6 +2,11 @@
 let originalString = "I love cats.";
 //step 2
 console.log("Original string:");
-console.log(originalString)
+console.log(originalString);
 //step 3
 let replacedString = originalString.replace("cats", "dogs");
+
+//step 4
+console.log("After using the replace() method:");
+console.log(replacedString);
+//step 5
