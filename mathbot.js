61c8beda016712fec461b67c2d7ae436c7607184
Peter Iregi <iregipeter4@gmail.com>
2026-07-14 08:44:54 +0300

    first use of math .random function

--- mathbot.js
+++ mathbot.js
@@ -1,3 +1,10 @@
+// step 1
 let botName ="MathBot";
 let greeting =`Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
 console.log(greeting);
+//step 2
+console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
+//step 3
+let randomNum = Math.random();
+
+console.log(randomNum);
