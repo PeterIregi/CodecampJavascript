8e3db3b4b1c342097a0684a071a9196a542d9ffc
Peter Iregi <iregipeter4@gmail.com>
2026-07-31 09:07:40 +0300

    Add a Square function

--- buildaCalculator.js
+++ buildaCalculator.js
@@ -29,3 +29,10 @@ function calculateQuotient(num1, num2) {
 
 console.log(calculateQuotient(7,11));
 console.log(calculateQuotient(3,0));
+
+function calculateSquare(num) {
+  return num ** 2;
+};
+
+console.log(calculateSquare(2));
+console.log(calculateSquare(9));
