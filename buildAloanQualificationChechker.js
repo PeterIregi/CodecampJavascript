25a30378a3d8fc0525f7199b332e953fd1515148
PeterIregi <iregipeter4@gmail.com>
2026-08-02 04:59:45 +0300

    Create variables needed for the exercise

---
+++ buildAloanQualificationChechker.js
@@ -0,0 +1,6 @@
+let minIncomeForDuplex = 60000;
+let minCreditScoreForDuplex = 700;
+let minIncomeForCondo = 45000;
+let minCreditScoreForCondo = 680;
+let minIncomeForCar = 30000;
+let minCreditScoreForCar = 650;
