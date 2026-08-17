37b16b2e3b480cb18402ad4a35628da6be90931b
Peter Iregi <iregipeter4@gmail.com>
2026-08-17 11:19:38 +0300

    Use pop method and view results

---
+++ shoppinglist.js
@@ -0,0 +1,13 @@
+console.log("Grocery shopping list");
+
+const shoppingList = [];
+
+console.log("It will be nice to have some fruit to eat.");
+
+shoppingList.push("Apples");
+
+function getShoppingListMsg(arr) {
+  return `Current Shopping List: ${arr}`;
+}
+
+console.log(getShoppingListMsg(shoppingList));
