d2fe20712197d6b5635c310b8150bb8238a27b0d
PeterIregi <iregipeter4@gmail.com>
2026-08-18 04:55:49 +0300

    Update list some more and display result

--- shoppinglist.js
+++ shoppinglist.js
@@ -29,3 +29,14 @@ console.log("This looks like too much junk food.");
 
 shoppingList.pop();
 console.log(getShoppingListMsg(shoppingList));
+console.log("It might be nice to get a dessert.");
+
+shoppingList.unshift("Chocolate Cake");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("On second thought, maybe we should be more health conscious.");
+
+shoppingList.shift();
+shoppingList[0] = "Canola Oil";
+
+console.log(getShoppingListMsg(shoppingList));
